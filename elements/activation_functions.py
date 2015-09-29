987085c0ae0c216aeb3e41ffa53a7655df136051
Mahdi Mohaveri <mmohaveri@gmail.com>
2015-09-30 00:10:15 +0330

    Convert all TABs to Spaces.

--- elements/activation_functions.py
+++ elements/activation_functions.py
@@ -8,57 +8,57 @@ to set initial value for some parameters.
 """
 
 def tanh(x):
-	"""
-	tanh function (-1 to 1)
+    """
+    tanh function (-1 to 1)
 
-	@input: x, theano shared variable.
-	@output: element-wise tanh of x
-	"""
-	return T.tanh(x)
+    @input: x, theano shared variable.
+    @output: element-wise tanh of x
+    """
+    return T.tanh(x)
 
 def sigmoid(x):
-	"""
-	sigmoid function (0 to 1, (tanh(x)+1)/2).
+    """
+    sigmoid function (0 to 1, (tanh(x)+1)/2).
 
-	@input: x, theano shared variable.
-	@output: element-wise sigmoid of x
-	"""
-	return (T.tanh(x)+1)/2
+    @input: x, theano shared variable.
+    @output: element-wise sigmoid of x
+    """
+    return (T.tanh(x)+1)/2
 
 def linier(x):
-	"""
-	linier function.
+    """
+    linier function.
 
-	@input: x, theano shared variable.
-	@output: x
-	"""
-	return x
+    @input: x, theano shared variable.
+    @output: x
+    """
+    return x
 
 def relu_generator(alpha=0):
-	"""
-	this function returns a relu function with proper alpha value.
+    """
+    this function returns a relu function with proper alpha value.
 
-	@input: alpha, slope of negative side of ReLU.
-	@output: ReLU function
-	"""
-	def relu(x):
-		"""
-		rectified linier function (-alpha*x if x<0, x if x>0).
+    @input: alpha, slope of negative side of ReLU.
+    @output: ReLU function
+    """
+    def relu(x):
+        """
+        rectified linier function (-alpha*x if x<0, x if x>0).
 
-		@input: x, theano shared variable.
-		@output: x<0?-alpha*x:x
-		"""
-		return T.nnet.relu(x, alpha)
+        @input: x, theano shared variable.
+        @output: x<0?-alpha*x:x
+        """
+        return T.nnet.relu(x, alpha)
 
-	return relu
+    return relu
 
 # TODO:
 # add RBF activation function
 #
 # def RBF(x):
-# 	"""
-# 	radial basis function.
+#   """
+#   radial basis function.
 
-# 	@input: x, theano shared variable.
-# 	@output: Not Implimented
-# 	"""
+#   @input: x, theano shared variable.
+#   @output: Not Implimented
+#   """
