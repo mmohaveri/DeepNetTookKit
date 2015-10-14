37b982028bfe557d0df15120e759dde63c1b71b8
Mahdi Mohaveri <mmohaveri@gmail.com>
2015-10-14 18:29:05 +0330

    Add plot drawing to ShallowNetwork.

--- networks/multi_layer_feed_forward.py
+++ networks/multi_layer_feed_forward.py
@@ -1,8 +1,11 @@
 import cPickle
+import collections
 import numpy
 import sys
 import theano
 import theano.tensor as T
+
+import matplotlib.pyplot as plt
 
 from abc import abstractmethod, ABCMeta
 
@@ -189,7 +192,10 @@ class ShallowNetwork(BaseNetwork):
 
         params_g = [T.grad(self.get_cost(), param) for param in self.get_params()]
 
+        # Normal SGD
         updates = [(param, param - learning_rate * param_g) for param, param_g in zip(self.get_params(), params_g)]
+
+        # Momentum SGD
 
         train_model = theano.function(inputs=[index], outputs=self.get_cost(), updates=updates,
                                       givens=
@@ -213,16 +219,22 @@ class ShallowNetwork(BaseNetwork):
         epoch = 0
         done_looping = False
 
+        train_error_history = collections.OrderedDict()
+        validation_error_history = collections.OrderedDict()
+
         while (epoch < n_epochs) and (not done_looping):
             epoch += 1
 
             for minibatch_index in xrange(n_train_batches):
-
-                minibatch_avg_cost = train_model(minibatch_index)
                 # iteration number
-                iter = (epoch - 1) * n_train_batches + minibatch_index
+                itteration_number = (epoch - 1) * n_train_batches + minibatch_index
 
-                if (iter + 1) % validation_frequency == 0:
+                # Save itteration history
+                minibatch_avg_cost = train_model(minibatch_index)
+                train_error_history[itteration_number] = minibatch_avg_cost
+
+                # validate every validation_frequency time :)
+                if (itteration_number + 1) % validation_frequency == 0:
                     # compute zero-one loss on validation set
                     validation_losses = [validate_model(i) for i in xrange(n_valid_batches)]
                     this_validation_loss = numpy.mean(validation_losses)
@@ -231,23 +243,30 @@ class ShallowNetwork(BaseNetwork):
                           (epoch, minibatch_index + 1, n_train_batches, this_validation_loss * 100.)),
                     sys.stdout.flush()
 
+                    # Save validation history
+                    validation_error_history[itteration_number] = this_validation_loss
+
                     # if we got the best validation score until now
                     if this_validation_loss < best_validation_loss:
                         # improve patience if loss improvement is good enough
                         if this_validation_loss < best_validation_loss * improvement_threshold:
-                            patience = max(patience, iter * patience_increase)
+                            patience = max(patience, itteration_number * patience_increase)
 
                         best_validation_loss = this_validation_loss
-                        best_iter = iter
+                        best_iter = itteration_number
 
                         # test it on the test set
                         test_losses = [test_model(i) for i in xrange(n_test_batches)]
                         test_score = numpy.mean(test_losses)
 
-                if patience <= iter:
+                if patience <= itteration_number:
                     done_looping = True
                     break
+        plt.plot(train_error_history.keys(), train_error_history.values(), 'r',
+                 validation_error_history.keys(), validation_error_history.values(), 'b')
 
+        print validation_error_history.keys()
+        plt.savefig("assets/outputs/image.jpg")
         print(('Optimization complete. Best validation score of %f %% '
                'obtained at iteration %i, with test performance %f %%') %
               (best_validation_loss * 100., best_iter + 1, test_score * 100.))
